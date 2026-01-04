037569945728a39d32d7fa9d43da205d408dbd5c
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2026-01-04 16:17:37 +0100

    add force-dynamic to backpack

--- src/app/backpack/page.jsx
+++ src/app/backpack/page.jsx
@@ -5,6 +5,8 @@ import { getCryptos } from "@/api/getCrypto";
 import CryptoList from "@/components/crypto-list/CryptoList";
 import Section from "@/shared/section/Section";
 
+export const dynamic = "force-dynamic";
+
 export default async function Backpack() {
   const cryptos = await getCryptos();
 
