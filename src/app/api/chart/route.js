d23dabd38635dd3afe1dda6e5c66304c071e91f0
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2026-02-20 17:54:58 +0100

    remove fetch action

--- src/app/api/chart/route.js
+++ src/app/api/chart/route.js
@@ -7,44 +7,49 @@ export async function GET(req) {
   if (!coin) {
     return Response.json({ error: "Missing coin param" }, { status: 400 });
   }
+  const res = await fetch(
+    `https://api.bybit.com/v5/market/kline?category=spot&symbol=${coin}&interval=${interval}&limit=1000`,
+    { cache: "no-store" },
+  );
+  return Response.json(res);
 
-  try {
-    const res = await fetch(
-      `https://api.bybit.com/v5/market/kline?category=spot&symbol=${coin}&interval=${interval}&limit=1000`,
-      { cache: "no-store" },
-    );
-    console.log("api/chart res :>> ", res);
-    if (!res.ok) {
-      return Response.json(
-        { error: "Bybit request failed", status: res.status },
-        { status: 500 },
-      );
-    }
+  // try {
+  //   const res = await fetch(
+  //     `https://api.bybit.com/v5/market/kline?category=spot&symbol=${coin}&interval=${interval}&limit=1000`,
+  //     { cache: "no-store" },
+  //   );
+  //   console.log("api/chart res :>> ", res);
+  //   if (!res.ok) {
+  //     return Response.json(
+  //       { error: "Bybit request failed", status: res.status },
+  //       { status: 500 },
+  //     );
+  //   }
 
-    let data;
-    try {
-      data = await res.json();
-    } catch (e) {
-      return Response.json(
-        { error: "Invalid JSON from Bybit", details: e.message },
-        { status: 500 },
-      );
-    }
+  //   let data;
+  //   try {
+  //     data = await res.json();
+  //   } catch (e) {
+  //     return Response.json(
+  //       { error: "Invalid JSON from Bybit", details: e.message },
+  //       { status: 500 },
+  //     );
+  //   }
 
-    if (!data?.result?.list) {
-      return Response.json(
-        { error: "Invalid Bybit response" },
-        { status: 500 },
-      );
-    }
+  //   if (!data?.result?.list) {
+  //     return Response.json(
+  //       { error: "Invalid Bybit response" },
+  //       { status: 500 },
+  //     );
+  //   }
 
-    const candles = createCandles(data.result.list);
-    return Response.json(candles);
-  } catch (error) {
-    console.error("API /chart error:", error?.message || error, error);
-    return Response.json(
-      { error: "Internal server error", details: error?.message || "Unknown" },
-      { status: 500 },
-    );
-  }
+  //   const candles = createCandles(data.result.list);
+  //   return Response.json(candles);
+  // } catch (error) {
+  //   console.error("API /chart error:", error?.message || error, error);
+  //   return Response.json(
+  //     { error: "Internal server error", details: error?.message || "Unknown" },
+  //     { status: 500 },
+  //   );
+  // }
 }
