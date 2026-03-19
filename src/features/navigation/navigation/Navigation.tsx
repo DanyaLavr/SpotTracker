0c32e8aa9c786bd463b471317f45c8f8364f9848
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2026-03-19 14:39:14 +0100

    feat: animations to navigation's items

--- src/features/navigation/navigation/Navigation.tsx
+++ src/features/navigation/navigation/Navigation.tsx
@@ -5,27 +5,38 @@ import { selectUser } from "@/entities/user/modules/redux/selectors";
 import { usePathname } from "next/navigation";
 import { useAppSelector } from "@/store/hooks";
 
+const LINK_STYLES = "relative py-3 inline-block";
+
 export default function Navigation() {
   const user = useAppSelector(selectUser);
   const pathname = usePathname();
-  const isActiveClass = (href: string) =>
-    pathname === href ? "p-3 pl-0 underline" : "p-3 pl-0";
+
+  const getLinkClass = (href: string) => {
+    const isActive = pathname === href;
+    return isActive
+      ? `${LINK_STYLES} underline`
+      : `${LINK_STYLES}  transition-transform duration-300 ease-in-out hover:scale-110 after:absolute after:bottom-2 after:left-0 after:h-0.5 after:w-0 after:rounded-full after:bg-current after:transition-all after:duration-300 after:ease-in-out hover:after:w-full`;
+  };
+
   return (
-    <nav className="md:text-white font-bold text-2xl lg:text-3xl ">
+    <nav className="md:text-white font-bold text-2xl lg:text-3xl">
       <ul className="grid gap-5 justify-items-center md:justify-items-start">
         <li>
-          <Link className={isActiveClass("/")} href="/">
+          <Link className={getLinkClass("/")} href="/">
             Home
           </Link>
         </li>
         <li>
-          <Link className={isActiveClass("/backpack")} href={`/backpack`}>
+          <Link className={getLinkClass("/backpack")} href="/backpack">
             Backpack
           </Link>
         </li>
         {!user && (
           <li>
-            <Link className="p-3 pl-0" href="/registration">
+            <Link
+              className={getLinkClass("/registration")}
+              href="/registration"
+            >
               Log in
             </Link>
           </li>
