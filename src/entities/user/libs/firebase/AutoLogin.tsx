a96c99e5a7a52abd9e4ff8a356166f4b53cc669d
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2026-04-07 13:17:05 +0200

    feat: onAuthStateChanged in AutoLogin

--- src/entities/user/libs/firebase/AutoLogin.tsx
+++ src/entities/user/libs/firebase/AutoLogin.tsx
@@ -11,65 +11,65 @@ export default function AutoLogin() {
   const dispatch = useAppDispatch();
   const router = useRouter();
 
-  useEffect(() => {
-    getRedirectResult(auth)
-      .then((result) => {
-        console.log("redirect result :>> ", result);
-        if (!result?.user) return;
-        const { uid, email, displayName } = result.user;
-        dispatch(
-          setUser({ uid, email: email ?? "", login: displayName ?? "" }),
-        );
-        router.replace("/");
-      })
-      .catch((e) => console.error("redirect error :>> ", e));
-  }, []);
   // useEffect(() => {
-  //   let isRedirect = false;
-
   //   getRedirectResult(auth)
-  //     .then((res) => {
-  //       console.log("getRedirectResult :>>", res);
-  //       if (!res) return;
-
-  //       isRedirect = true;
-  //       const user = res.user;
+  //     .then((result) => {
+  //       console.log("redirect result :>> ", result);
+  //       if (!result?.user) return;
+  //       const { uid, email, displayName } = result.user;
   //       dispatch(
-  //         setUser({
-  //           uid: user.uid,
-  //           email: user.email ?? "",
-  //           login: user.displayName ?? "",
-  //         }),
+  //         setUser({ uid, email: email ?? "", login: displayName ?? "" }),
   //       );
-
   //       router.replace("/");
-  //       router.refresh();
   //     })
-  //     .catch(console.error);
-  //   const getter = async () => {
-  //     const res = await getRedirectResult(auth);
-  //     console.log("res :>> ", res);
-  //   };
-  //   getter();
-  //   // onAuthStateChanged — страховка, сработает в любом случае
-  //   const unsubscribe = onAuthStateChanged(auth, (user) => {
-  //     console.log("onAuthStateChanged user :>>", user);
-
-  //     if (isRedirect) return;
-
-  //     if (user) {
-  //       dispatch(
-  //         setUser({
-  //           uid: user.uid,
-  //           email: user.email ?? "",
-  //           login: user.displayName ?? "",
-  //         }),
-  //       );
-  //     }
-  //   });
-
-  //   return () => unsubscribe();
+  //     .catch((e) => console.error("redirect error :>> ", e));
   // }, []);
+  useEffect(() => {
+    let isRedirect = false;
+
+    getRedirectResult(auth)
+      .then((res) => {
+        console.log("getRedirectResult :>>", res);
+        if (!res) return;
+
+        isRedirect = true;
+        const user = res.user;
+        dispatch(
+          setUser({
+            uid: user.uid,
+            email: user.email ?? "",
+            login: user.displayName ?? "",
+          }),
+        );
+
+        router.replace("/");
+        router.refresh();
+      })
+      .catch(console.error);
+    const getter = async () => {
+      const res = await getRedirectResult(auth);
+      console.log("res :>> ", res);
+    };
+    getter();
+    // onAuthStateChanged — страховка, сработает в любом случае
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log("onAuthStateChanged user :>>", user);
+
+      if (isRedirect) return;
+
+      if (user) {
+        dispatch(
+          setUser({
+            uid: user.uid,
+            email: user.email ?? "",
+            login: user.displayName ?? "",
+          }),
+        );
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return null;
 }
